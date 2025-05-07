338feee0c3890efe576e276dcae13e2a51062d46
sintaxis9 <skizosyntax@duck.com>
2025-05-07 14:16:53 -0400

    some validations

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.11.0-slim
-WORKDIR /app
-
-COPY requirements.txt .
-COPY src/ src/
-
-RUN pip install --no-cache-dir -r requirements.txt
-
-CMD ["python", "src/main.py"]
--- src/matrix_operations/validations.py
+++ src/matrix_operations/validations.py
@@ -1,2 +1,27 @@
-def is_square(matrix):
+def is_pair(matrix):
     return len(matrix) == len(matrix[0]) if matrix else False
+
+def is_identity(matrix):
+    return all(matrix[i][i] == 1 for i in range(len(matrix)))
+
+def is_multipliable(matrix1, matrix2):
+    if len(matrix1[0]) == len(matrix2):
+        return True
+    else: False
+
+def is_inversible(matrix):
+    if not is_pair(matrix):
+        return False
+
+    def determinant(matrix):
+        if len(matrix) == 2:
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+            
+        det = 0
+        for c in range(len(matrix)):
+            submatrix = [file[:c] + file[c+1:] for file in matrix[1:]]
+            sign = (-1) ** c
+            det += sign * matrix[0][c] * determinant(submatrix)
+        return det
+    
+    return determinant(matrix) != 0
