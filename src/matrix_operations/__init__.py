3d324c227f2ed7339e009f30d271acf8f5940f99
sintaxis9 <skizosyntax@duck.com>
2025-05-08 01:27:18 -0400

    init calcs

---
+++ src/matrix_operations/__init__.py
@@ -0,0 +1,3 @@
+from .matrix_ops import add, subtract, multiply, determinant, inverse
+from .validations import is_invertible, is_identity, have_same_dimensions, is_multipliable
+from .core_utils import is_square, is_numeric
