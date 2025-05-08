edabdeccdb1a593c0e27a81a0c67b51db5ce4f3c
sintaxis9 <skizosyntax@duck.com>
2025-05-08 00:42:44 -0400

    add, substract and multiply operations

--- src/matrix_operations/matrix_ops.py
+++ src/matrix_operations/matrix_ops.py
@@ -1,4 +1,38 @@
 from core_utils import is_square, is_numeric
+from validations import is_multipliable, have_same_dimensions
+
+
+def add(matrix1, matrix2):
+    if not have_same_dimensions(matrix1, matrix2):
+        raise ValueError("the matrix must have the same dimensions")
+    
+    return[
+        [matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))]
+        for i in range(len(matrix1))
+    ]
+
+def subtract(matrix1, matrix2):
+    if not have_same_dimensions(matrix1, matrix2):
+        raise ValueError("the matrix must have the same dimensions")
+    
+    return[
+        [matrix1[i][j] - matrix2[i][j] for j in range(len(matrix1[0]))]
+        for i in range(len(matrix1))
+    ]
+
+
+def multiply(matrix1, matrix2):
+    if not is_multipliable(matrix1, matrix2):
+        return None
+    
+    m, n, p = len(matrix1), len(matrix2[0]), len(matrix2)
+    result = [[0 for _ in range(n)] for _ in range(m)]
+
+    for i in range(m):
+        for j in range(n):
+            for k in range(p):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+    return result
 
 
 def determinant(matrix):
